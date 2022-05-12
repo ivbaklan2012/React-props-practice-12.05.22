03c24e07f00d11f3243bf1da20d4b62a071a1779
Ivan Baklan <51780841+ivbaklan2012@users.noreply.github.com>
2022-05-12 13:14:54 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
